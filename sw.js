bda016ecac00f6f6d682094cf34c36761e56afc8
xjl <415737101@qq.com>
2026-04-29 20:37:27 +0800

    Updates

--- sw.js
+++ sw.js
@@ -1,8 +1,19 @@
-const CACHE = 'thought-journal-v1';
-const ASSETS = ['/', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+const CACHE = 'thought-journal-v2';
+const BASE = '/thought-journal';
+const ASSETS = [BASE + '/', BASE + '/manifest.json', BASE + '/icons/icon-192.png', BASE + '/icons/icon-512.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
